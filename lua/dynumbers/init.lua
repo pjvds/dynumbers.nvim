33146d0f1cb76abd80a0120f072d00615419a206
Pieter Joost van de Sande <pj@born2code.net>
2023-04-08 12:39:54 +0200

    feat: add logging and use relative number in visual mode

--- lua/dynumbers/init.lua
+++ lua/dynumbers/init.lua
@@ -1,25 +1,59 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local group = augroup("toggling-numbers", { clear = true })
+local log = require("dynumbers.log").new({
+	plugin = "dynumbers.nvim",
+	level = "warn",
+})
 
----@param opts {}
+assert(log, "failed to create logger")
+
+local function evaluate()
+	local mode = vim.api.nvim_get_mode().mode
+	local relative = mode == "n" or mode == "v"
+
+	if relative then
+		log.trace("switching to relative numbers because mode: ", mode)
+	else
+		log.trace("switching to absolute numbers because mode: ", mode)
+	end
+
+	vim.wo.relativenumber = relative
+end
+
+---@param opts { log_level: string }
 local function setup(opts)
+	if opts.log_level then
+		log.level = opts.log_level
+	end
+
 	vim.o.number = true
 	vim.o.relativenumber = true
 
-	autocmd("BufLeave,FocusLost,InsertEnter,WinLeave", {
+	autocmd({ "BufLeave", "FocusLost", "WinLeave" }, {
 		pattern = "*",
 		group = group,
-		callback = function()
-			vim.o.relativenumber = false
+		callback = function(event)
+			log.trace("switching to absolute numbers because event: ", event)
+			vim.wo.relativenumber = false
+		end,
+	})
+
+	autocmd({ "BufEnter", "FocusGained", "WinEnter" }, {
+		pattern = { "*" },
+		group = group,
+		callback = function(event)
+			log.trace("evaluating numbers because event: ", event)
+			evaluate()
 		end,
 	})
 
 	autocmd("ModeChanged", {
-		pattern = { "*:i*", "i*:*" },
+		pattern = { "*:*" },
 		group = group,
-		callback = function()
-			vim.o.relativenumber = vim.v.event.new_mode:match("^i") == nil
+		callback = function(event)
+			log.trace("evaluating numbers because event: ", event)
+			evaluate()
 		end,
 	})
 end
